ace4debbb03709a603893b1c0123c08f82a85502
Mohitkumar3282 <157482076+Mohitkumar3282@users.noreply.github.com>
2026-08-15 15:26:39 +0530

    add

--- backend/src/common/config/env.js
+++ backend/src/common/config/env.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 module.exports = {
   PORT: process.env.PORT || 5005,
-  MONGO_URI: process.env.MONGO_URI || 'mongodb://localhost:27017/multi-service-db',
+  MONGO_URI: process.env.MONGO_URI || 'mongodb+srv://prathmeshjawade2_db_user:qWDWRZ1farEr1BKt@cluster0.k6c7d8j.mongodb.net/theshine?appName=Cluster0',
   JWT_SECRET: process.env.JWT_SECRET || 'tsl_jwt_secret_key_2026_shine_lounge',
   ADMIN_EMAIL: process.env.ADMIN_EMAIL || 'admin@gmail.com',
   ADMIN_PASSWORD: process.env.ADMIN_PASSWORD || 'Admin!@#123'
